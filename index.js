057f67a22223458860e2337da01ee7e9b84088b4
siddharthvaddem <siddharthvaddem@yahoo.com>
2020-08-30 19:13:30 +0530

    product validation added,product route fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
 );
 
 app.use(authRouter);
-
+app.use(productsRouter);
 app.listen(3000, () => {
 	console.log('listening');
 });
--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,10 +4,10 @@ const { getError } = require('../../helpers');
 module.exports = ({ errors }) => {
 	return layout({
 		content: `
-        <form method="POST">
-            <input placeholder="Title" name="title">
-            <input placeholder="Price" name="price">
-            <input tyoe="file" name="image"/>
+        <form method="POST" enctype="multipart/form-data">
+            <input placeholder="Title" name="title"/>
+            <input placeholder="Price" name="price"/>
+            <input type="file" name="image"/>
             <button>Submit</button>
         </form>`
 	});
